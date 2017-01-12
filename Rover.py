f2cb2cc2cea4e7ae788711d920d8d29c8608f49e
Yusuf Osman <yusuf.o@gmail.com>
2017-01-12 11:55:05 +0000

    -Added basic turning left and right tests -Added basic turning left and right functionality

--- Rover.py
+++ Rover.py
@@ -1,8 +1,26 @@
 position = [0, 0, "N"]
 
+def calculate_new_direction(facing, direction):
+    combined = facing+direction
+    direction_dictionary = {
+        "NR": "E",
+        "NL": "W",
+        "ER": "S",
+        "EL": "E",
+        "SR": "W",
+        "SL": "E",
+        "WR": "N",
+        "WL": "S",
+    }
+
+    return direction_dictionary.get(combined, "error")
+
 
 def move_rover(direction):
     if direction == "F":
         return [0, position[1] + 1, "N"]
     elif direction == "B":
         return [0, position[1] - 1, "N"]
+    elif direction == "L" or direction == "R":
+        new_direction = calculate_new_direction(position[2], direction)
+        return [0, position[1], new_direction]
--- TestRover.py
+++ TestRover.py
@@ -15,5 +15,15 @@ class MoveTests(unittest.TestCase):
         expected = [0, -1, "N"]
         self.assertEqual(expected, result)
 
+    def test_move_left(self):
+        result = Rover.move_rover("L")
+        expected = [0, 0, "W"]
+        self.assertEqual(expected, result)
+
+    def test_move_right(self):
+        result = Rover.move_rover("R")
+        expected = [0, 0, "E"]
+        self.assertEqual(expected, result)
+
 if __name__ == '__main__':
     unittest.main()
