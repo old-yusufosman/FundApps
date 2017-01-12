379fe8cc9db744b9ac880291a67675435f24b22e
Yusuf Osman <yusuf.o@gmail.com>
2017-01-12 13:02:43 +0000

    Set up Rover class and improved movement logic

--- Rover.py
+++ Rover.py
@@ -1,8 +1,8 @@
-position = [0, 0, "N"]
 grid_size = 10
 
+
 def calculate_new_direction(facing, direction):
-    combined = facing+direction
+    combined = facing + direction
     direction_dictionary = {
         "NR": "E",
         "NL": "W",
@@ -13,15 +13,32 @@ def calculate_new_direction(facing, direction):
         "WR": "N",
         "WL": "S",
     }
+    return direction_dictionary.get(combined, "key {} not found".format(combined))
 
-    return direction_dictionary.get(combined, "error")
+
+def calculate_new_position(position, direction):
+    combined = position[2] + direction
+    direction_dictionary = {
+        "NF": [position[0], (position[1] + 1) % grid_size, position[2]],
+        "NB": [position[0], (position[1] - 1) % grid_size, position[2]],
+        "EF": [(position[0] + 1) % grid_size, position[1], position[2]],
+        "EB": [(position[0] - 1) % grid_size, position[1], position[2]],
+        "SF": [position[0], (position[1] - 1) % grid_size, position[2]],
+        "SB": [position[0], (position[1] + 1) % grid_size, position[2]],
+        "WF": [(position[0] - 1) % grid_size, position[1], position[2]],
+        "WB": [(position[0] + 1) % grid_size, position[1], position[2]],
+    }
+    return direction_dictionary.get(combined, ["key {} not found".format(combined)])
 
 
-def move_rover(direction):
-    if direction == "F":
-        return [0, position[1] + 1, "N"]
-    elif direction == "B":
-        return [0, position[1] - 1, "N"]
-    elif direction == "L" or direction == "R":
-        new_direction = calculate_new_direction(position[2], direction)
-        return [0, position[1], new_direction]
+class Rover:
+    def __init__(self):
+        self.position = [0, 0, "N"]
+
+    def move_rover(self, direction):
+        if direction == "F" or direction == "B":
+            new_position = calculate_new_position(self.position, direction)
+            return new_position
+        elif direction == "L" or direction == "R":
+            new_direction = calculate_new_direction(self.position[2], direction)
+            return [0, self.position[1], new_direction]
--- TestRover.py
+++ TestRover.py
@@ -3,24 +3,39 @@ import Rover
 
 
 class MoveTests(unittest.TestCase):
+    def setUp(self):
+        self.Rover = Rover.Rover()
+
     def test_move_north(self):
-        result = Rover.move_rover("F")
+        result = self.Rover.move_rover("F")
         expected = [0, 1, "N"]
         self.assertEqual(expected, result)
 
     def test_move_south(self):
-        result = Rover.move_rover("B")
-        expected = [0, -1, "N"]
+        self.Rover.position = [0, 1, "N"]
+        result = self.Rover.move_rover("B")
+        expected = [0, 0, "N"]
         self.assertEqual(expected, result)
 
     def test_move_left(self):
-        result = Rover.move_rover("L")
+        result = self.Rover.move_rover("L")
         expected = [0, 0, "W"]
         self.assertEqual(expected, result)
 
     def test_move_right(self):
-        result = Rover.move_rover("R")
+        result = self.Rover.move_rover("R")
         expected = [0, 0, "E"]
+        self.assertEqual(expected, result)
+
+    def test_wrap_left(self):
+        self.Rover.position = [0, 0, "W"]
+        result = self.Rover.move_rover("F")
+        expected = [9, 0, "W"]
+        self.assertEqual(expected, result)
+
+    def test_wrap_bottom(self):
+        result = self.Rover.move_rover("B")
+        expected = [0, 9, "N"]
         self.assertEqual(expected, result)
 
 if __name__ == '__main__':
